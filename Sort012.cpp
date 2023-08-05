c693692e3fcd34e4af47c59884e80ae5a49b1ee7
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2023-08-05 18:10:53 +0530

    Create Sort012.cpp

---
+++ Sort012.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        int zero =0, one = 0, two=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0) zero++;
+            else if(a[i]==1) one++;
+            else two++;
+        }
+        for(int i=0;i<zero;i++) a[i] = 0;
+        for(int i=zero;i<zero+one;i++) a[i]= 1;
+        for(int i=zero+one;i<zero+one+two;i++) a[i] = 2;
+    }
+    
+};
