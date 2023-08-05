ec6b2cee11f79997f0eff96d36fdf3326d343dbd
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2023-08-05 18:16:28 +0530

    Create Minimize the Heights II.cpp

---
+++ Minimize the Heights II.cpp
@@ -0,0 +1,23 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        if(n==1)
+        return 0;
+        sort(arr,arr+n);
+        int ans = arr[n-1]-arr[0];
+        int smallest = arr[0]+k;
+        int largest = arr[n-1]-k;
+        int mi,ma;
+        for(int i=0;i<n-1;i++)
+        {
+            mi = min(smallest,arr[i+1]-k);
+            ma = max(largest,arr[i]+k);
+            if(mi<0)continue;
+            ans = min(ans,ma-mi);
+        }
+        return ans;
+    }
+};
