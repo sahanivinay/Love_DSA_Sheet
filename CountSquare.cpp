ec84f3990743ec17194cb9fa7d930a2e8203df1f
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2023-08-05 18:27:32 +0530

    Create CountSquare.cpp

---
+++ CountSquare.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        int temp = sqrt(N);
+        if(temp*temp==N) return temp-1;
+        else return temp;
+    }
+};
