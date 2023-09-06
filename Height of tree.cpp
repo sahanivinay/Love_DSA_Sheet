050ec5da4517aa9dc609e82ee44389b287482fc5
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2023-09-06 14:38:05 +0530

    Create Height of tree.cpp

---
+++ Height of tree.cpp
@@ -0,0 +1,12 @@
+int height(struct Node* node){
+        // code here 
+        //Base case
+        if(node == NULL){
+            return 0;
+        }
+        int left = height(node->left);
+        int right = height(node->right);
+        int ans = max(left,right)+1;
+        return ans;
+        
+    }
